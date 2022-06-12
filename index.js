87303a9a21b13d1af585c6fc81678b24d8efffc6
OnanThairu <thairuonan@gmail.com>
2022-06-12 12:11:23 +0300

    .find lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    let val = array.find(obj => obj.result === "W")
+    if (val) {
+        return val.year
+    } else {
+        return undefined
+    }
+}
